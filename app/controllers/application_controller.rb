6ea9eea7ba3afcb86332014172fa2945aa239e8a
thomastrzos <trzos.tomasz@gmail.com>
2020-03-24 19:19:04 +0100

    Add index to ApplicationController, root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  def index
+    render plain: "Webcrawler by Tomasz Trzos. Environment: #{Rails.env}"
+  end
 end
